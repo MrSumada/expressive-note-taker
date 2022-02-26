588fa391ac36f59e5e24029e13243821dec00d55
MrSumada <ricksamericana.com>
2022-02-26 15:58:12 -0500

    add get for db and heroku

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const { animals } = require('./data/animals');
+const { notes } = require('./db/db');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+// const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 // Use apiRoutes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
+
+app.get('/api/animals', (req, res) => {
+  res.json(animals);
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
