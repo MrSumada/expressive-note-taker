772eb06e3cd9710a415216cde223c454779ffda3
MrSumada <ricksamericana.com>
2022-03-01 22:19:37 -0500

    fixed post ids, and delete

--- server.js
+++ server.js
@@ -2,21 +2,17 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
-const { notes } = require('./db/db');
+const { notes } = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Use apiRoutes
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
 
+// Create a new note, used for POST 
 function createNewNote(body, notesArray) {
 
   const note = body;
@@ -30,18 +26,7 @@ function createNewNote(body, notesArray) {
   return note;
 }
 
-// function deleteNote(body, notesArray) {
-
-//   const { id } = body.params;
-//   const notesIndex = notes.findIndex(p => p.id == id);
-
-//   notes.splice(notesIndex, 1);
-
-//   return res.json();
-
-
-// }
-
+// Validate a new note, used for POST
 function validateNote(note) {
   if (!note.title || typeof note.title !== "string") {
     return false;
@@ -71,7 +56,11 @@ app.get('*', (req, res) => {
 
 app.post('/api/notes', (req, res) => {
 
-    req.body.id = notes.length.toString();
+    // get unique id based on last note added 
+    // (rather than the array length, which can vary depending on deletions)
+    const lastEl = notes[notes.length - 1];
+    const lastId = parseInt(lastEl.id) + 1;
+    req.body.id = lastId.toString();
 
     if (!validateNote(req.body)) {
       res.status(400).send("Be sure to type out your note and give it a title!");
@@ -83,39 +72,26 @@ app.post('/api/notes', (req, res) => {
 
 app.delete('/api/notes/:id', (req, res) => {
 
+  // read old array
   fs.readFile(__dirname + '/db/db.json', (err, data) => {
-    
     if (err) {
       console.error(err);
       res.sendStatus(500);
       return;
     }
+
+    // find index based on id, and splice index
     const { id } = req.params;
-    
-    let newNotesArray = JSON.parse(data);
-    
-    console.log(newNotesArray.notes)
-    newNotesArray.notes.splice(id, 1);
-    console.log(newNotesArray.notes);
+    const index = notes.findIndex(note => note.id === id);
+    notes.splice(index, 1);
 
-    // for (var i = 0; i < newNotesArray.length; i++) {
-
-    //   if (newNotesArray[i].id === id) {
-
-    //     newNotesArray.splice(i, 1);
-        
-    //     return;
-    //   }
-      
-    // }
-
-    fs.writeFile('./db/db.json', JSON.stringify({ notes: newNotesArray.notes }, null, 4), (err) => {
+    // write new array
+    fs.writeFile('./db/db.json', JSON.stringify({ notes: notes }, null, 4), (err) => {
       if (err) {
         res.sendStatus(500);
         return;
       }
-      // console.log(newNotesArray.notes);
-      res.json(newNotesArray);
+      res.json(notes);
     });
   });
 });
